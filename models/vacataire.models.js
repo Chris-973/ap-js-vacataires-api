4bbb042c9b5a58d31cd691fc4e190589e85b6f6c
Chris Marie-Angélique <christopher2211.angelique@gmail.com>
2023-09-27 18:11:11 +0200

    inital commit

---
+++ models/vacataire.models.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+
+const vacataireSchema = mongoose.Schema(
+    {
+        message: {
+            type: String,
+            required: true
+        },
+        author: {
+            type: String
+        },
+        likers: {
+            type: [String]
+        }
+
+        // name: {
+        //     type: String,
+        //     required: true
+        // },
+        // lastName: {
+        //     type: String,
+        //     required: true
+        // },
+        // email: {
+        //     type: String,
+        //     required: true
+        // },
+        // department: {
+        //     type: [String]
+        // },
+        // skills: {
+        //     type: [String]
+        // },
+        // socials: {
+        //     type: [String]
+        // },
+        // status: {
+        //     type: String
+        // },
+    }
+)
+
+module.exports = mongoose.model('vacataire', vacataireSchema)
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv").config();
+const port = 3000;
+
+const cors = require('cors');
+
+// connexion à la DB
+connectDB();
+
+const app = express();
+
+app.use(cors());
+
+// Middleware qui permet de traiter les données de la Request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/vacataires", require("./routes/vacataires.routes"));
+
+// Lancer le serveur
+app.listen(port, () => console.log("Le serveur a démarré au port  " + port));
