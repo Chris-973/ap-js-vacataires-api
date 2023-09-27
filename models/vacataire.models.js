21c504a9a1592ca3a826013566007fe5d11bb999
Chris Marie-Angélique <christopher2211.angelique@gmail.com>
2023-09-27 23:21:12 +0200

    edit files

--- models/vacataire.models.js
+++ models/vacataire.models.js
@@ -2,41 +2,43 @@ const mongoose = require("mongoose")
 
 const vacataireSchema = mongoose.Schema(
     {
-        message: {
+        // message: {
+        //     type: String,
+        //     required: true
+        // },
+        // author: {
+        //     type: String
+        // },
+        // likers: {
+        //     type: [String]
+        // },
+
+
+
+        name: {
             type: String,
             required: true
         },
-        author: {
+        lastName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        department: {
+            type: [String]
+        },
+        skills: {
+            type: [String]
+        },
+        socials: {
+            type: [String]
+        },
+        status: {
             type: String
         },
-        likers: {
-            type: [String]
-        }
-
-        // name: {
-        //     type: String,
-        //     required: true
-        // },
-        // lastName: {
-        //     type: String,
-        //     required: true
-        // },
-        // email: {
-        //     type: String,
-        //     required: true
-        // },
-        // department: {
-        //     type: [String]
-        // },
-        // skills: {
-        //     type: [String]
-        // },
-        // socials: {
-        //     type: [String]
-        // },
-        // status: {
-        //     type: String
-        // },
     }
 )
 
